3f5ac4927be1d292dc095dbc3099c392f63bb5e3
kazuhitonakayama <firstjin19990101@gmail.com>
2021-06-28 18:39:11 +0900

    Editting ArticleService

--- src/main/java/com/pomodoro/domain/article/service/ArticleService.java
+++ src/main/java/com/pomodoro/domain/article/service/ArticleService.java
@@ -1,10 +1,16 @@
 package com.pomodoro.domain.article.service;
 
 import com.pomodoro.domain.article.model.MArticle;
+import java.util.List;
 
 public interface ArticleService {
     /**
      * 記事登録
      */
     public void postPage(MArticle article);
+
+    /**
+     * 記事取得
+     */
+    public List<MArticle> getUsers();
 }
