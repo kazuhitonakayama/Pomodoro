c02390562e4b16c83d310a4c366c26d676975748
kazuhitonakayama <firstjin19990101@gmail.com>
2021-07-02 15:59:39 +0900

    Diet Forms

--- src/main/java/com/pomodoro/controller/ArticleController.java
+++ src/main/java/com/pomodoro/controller/ArticleController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import com.pomodoro.domain.article.model.MArticle;
 import com.pomodoro.domain.article.service.ArticleService;
-import com.pomodoro.form.EditForm;
-import com.pomodoro.form.PostForm;
+import com.pomodoro.form.ArticleForm;
 import org.springframework.ui.Model;
 
 import lombok.extern.slf4j.Slf4j;
@@ -29,12 +28,12 @@ public class ArticleController {
      */
     // 新規投稿画面を表示
     @GetMapping("/new")
-    public String getPost(@ModelAttribute PostForm form) {
+    public String getPost(@ModelAttribute ArticleForm form) {
         return "circles/new";
     }
     // 新規投稿処理
     @PostMapping("/new")
-    public String postRequest(@ModelAttribute PostForm form) {
+    public String postRequest(@ModelAttribute ArticleForm form) {
         //ログを表示
         log.info(form.toString());
 
@@ -54,15 +53,15 @@ public class ArticleController {
      */
     // 編集画面を表示
     @GetMapping("/edit/{id}")
-    public String editPost(@ModelAttribute EditForm form,Model model,@PathVariable("id") int id) {
+    public String editPost(@ModelAttribute ArticleForm form,Model model,@PathVariable("id") int id) {
         MArticle article = articleservice.getArticleOne(id);
-        form = modelMapper.map(article, EditForm.class);
-        model.addAttribute("editForm", form);
+        form = modelMapper.map(article, ArticleForm.class);
+        model.addAttribute("articleForm", form);
         return "circles/edit";
     }
     // 編集処理
     @PostMapping(value = "/edit" , params = "update")
-    public String updateArticle(EditForm form,Model model) {
+    public String updateArticle(ArticleForm form,Model model) {
         articleservice.updateArticleOne(form.getId(), form.getClubName(), form.getTitle(), form.getBody());
         return "redirect:/";
     }
@@ -71,7 +70,7 @@ public class ArticleController {
      * 記事削除処理
      */
     @PostMapping(value = "/edit" , params = "delete")
-    public String deleteArticle(EditForm form, Model model) {
+    public String deleteArticle(ArticleForm form, Model model) {
         articleservice.deletePage(form.getId());
         return "redirect:/";
     }
