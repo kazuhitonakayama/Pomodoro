1e79914e53fb932db9add0c76266f277996a9cd9
furude.K <hurudestars@gmail.com>
2021-07-05 19:12:14 +0900

    Refactored clubName to circleName

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS circles(
     id INT(100) DEFAULT 0 NOT NULL AUTO_INCREMENT,
-    clubName VARCHAR (50),
+    circleName VARCHAR (50),
     description VARCHAR (50),
     PRIMARY KEY(id)
     );
