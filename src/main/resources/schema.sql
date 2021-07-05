f340123295b635b597955b7de3acbf620a29b5aa
furude.K <hurudestars@gmail.com>
2021-07-05 13:56:30 +0900

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,10 @@ CREATE TABLE IF NOT EXISTS article(
     title VARCHAR (100),
     body VARCHAR (1000)
     );
+
+CREATE TABLE IF NOT EXISTS circles(
+--     FIXME:idのint(100)を可変に可能か調べる。
+    id INT(100) DEFAULT 0 NOT NULL AUTO_INCREMENT,
+    clubName VARCHAR (50),
+    description VARCHAR (50)
+    );
