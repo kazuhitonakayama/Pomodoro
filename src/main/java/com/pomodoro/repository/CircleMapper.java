916928df42c6d353e393d5f7f639df2dc6dfc533
furude.K <hurudestars@gmail.com>
2021-07-08 14:08:30 +0900

    Refactored file name to articles

--- src/main/java/com/pomodoro/repository/CircleMapper.java
+++ src/main/java/com/pomodoro/repository/CircleMapper.java
@@ -5,7 +5,7 @@ import org.apache.ibatis.annotations.Param;
 
 import com.pomodoro.domain.circle.model.MCircle;
 import java.util.List;
-
+//NOTE:現在このクラスは使用されていません。
 @Mapper
 public interface CircleMapper {
     // /** 記事登録 */
@@ -14,8 +14,8 @@ public interface CircleMapper {
     // /** 記事取得 */
     // public List<MArticle> findMany();
 
-    /** サークル1件取得 */
-    public MCircle findCircleName(int id);
+//    /** サークル1件取得 */
+//    public MCircle findCircleName(int id);
 
     // /** 記事削除(1件) */
     // public int deleteOne(@Param("id") int id);
