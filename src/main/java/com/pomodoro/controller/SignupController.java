1032c0aaf062328dacb3c8ef4926ef774921c378
furude.K <hurudestars@gmail.com>
2021-07-10 16:23:25 +0900

    Done until 83 page

--- src/main/java/com/pomodoro/controller/SignupController.java
+++ src/main/java/com/pomodoro/controller/SignupController.java
@@ -1,24 +1,31 @@
 package com.pomodoro.controller;
 
 import com.pomodoro.application.service.UserApplicationService;
+import com.pomodoro.form.SignupForm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Locale;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class SignupController {
     @Autowired
     private UserApplicationService userApplicationService;
 
-    /** ユーザー登録画面を表示*/
+    /**
+     * ユーザー登録画面を表示
+     */
     @GetMapping("/signup")
-    public String getSignup(Model model){
+    public String getSignup(Model model,  @ModelAttribute SignupForm form) {
         //性別を取得
         Map<String, Integer> genderMap = userApplicationService.getGenderMap();
         model.addAttribute("genderMap", genderMap);
@@ -27,10 +34,13 @@ public class SignupController {
         return "user/signup";
     }
 
-    /** ユーザー登録処理 */
+    /**
+     * ユーザー登録処理
+     */
     @PostMapping("/signup")
-    public String postSignup(){
-        //ログイン画面にリダイレクト
+    public String postSignup(@ModelAttribute SignupForm form) {
+        log.info(form.toString()); //slf4jを使ってform入力のログを表示
+        // ログイン画面にリダイレクト
         return "redirect:/login";
     }
 
