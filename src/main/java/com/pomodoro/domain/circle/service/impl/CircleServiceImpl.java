5e3d129a4ea5cbf925cf9a0a2d77fde3987d64af
kazuhitonakayama <firstjin19990101@gmail.com>
2021-07-15 15:17:51 +0900

    make Service Interface

--- src/main/java/com/pomodoro/domain/circle/service/impl/CircleServiceImpl.java
+++ src/main/java/com/pomodoro/domain/circle/service/impl/CircleServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pomodoro.domain.circle.model.MCircle;
+import com.pomodoro.domain.article.model.MArticle;
 import com.pomodoro.domain.circle.service.CircleService;
 import com.pomodoro.repository.CircleMapper;
 
@@ -31,11 +32,27 @@ public class CircleServiceImpl implements CircleService {
         return mapper.findMany();
     }
 
-//    /** 記事取得（1件）*/
-   @Override
-   public MCircle getCircleOne(int id) {
-       return mapper.findCircleOne(id);
-   }
+    /**
+     * サークルの活動一覧取得
+     */
+    @Override
+    public List<MArticle> getArticles(int id) {
+        return mapper.findArticlesOfCircle(id);
+    }
+    
+    /**
+     * サークルの名前取得 @ サークル詳細ページ
+     */
+    @Override
+    public String getCircleName(int id) {
+        return mapper.findCircleName(id);
+    }
+
+    //    /** 記事取得（1件）*/
+    @Override
+    public MCircle getCircleOne(int id) {
+        return mapper.findCircleOne(id);
+    }
 
 
      /** 記事削除（1件）*/
