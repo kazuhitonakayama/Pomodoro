d1696fad6a09d8435b85b35697c3e9efaf448588
furude.K <hurudestars@gmail.com>
2021-07-20 14:43:43 +0900

    Login btn for move to top page

--- src/main/java/com/pomodoro/controller/LoginController.java
+++ src/main/java/com/pomodoro/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.pomodoro.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -10,4 +11,13 @@ public class LoginController {
     public String getLogin(){
         return "login/login";
     }
+
+    /**
+     * トップ画面にリダイレクト
+     */
+    @PostMapping("/login")
+    public String postLogin() {
+        return "redirect:/";
+    }
 }
+
